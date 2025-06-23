ecd8884d4db219b80e8d379ea36ef8020e4e303f
Alberto Soto Fernandez <albert.soto@gmail.com>
2025-06-23 13:01:22 +0200

    solve spark init

--- decisiontree-backend/src/main/java/es/albertosoto/spark_dtree/configuration/SparkConfig.java
+++ decisiontree-backend/src/main/java/es/albertosoto/spark_dtree/configuration/SparkConfig.java
@@ -1,9 +1,13 @@
 package es.albertosoto.spark_dtree.configuration;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.SparkSession;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
 
 /**
  * Created by Alberto Soto. 23/6/25
@@ -13,16 +17,28 @@ public class SparkConfig {
 
     @Value("${spark.app.name}")
     private String appName;
-    
+
     @Value("${spark.master}")
     private String masterUrl;
 
     @Bean
-    public SparkSession sparkSession() {
-        return SparkSession
-                .builder()
-                .appName(appName)
-                .master(masterUrl)
+    public SparkConf sparkConf() throws IOException {
+        var conf = new SparkConf()
+                .setAppName(appName)
+                .setMaster(masterUrl);
+        return conf;
+    }
+
+    @Bean
+    public SparkSession sparkSession(JavaSparkContext sparkContext) {
+        return SparkSession.builder()
+                .sparkContext(sparkContext.sc())
+                .config(sparkContext.getConf())
                 .getOrCreate();
+    }
+
+    @Bean
+    public JavaSparkContext javaSparkContext(SparkConf sparkConf) {
+        return new JavaSparkContext(sparkConf);
     }
 }
