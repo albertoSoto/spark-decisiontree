3ac8a48228fa907fd7cbe44bf071ad6803db1803
Alberto Soto Fernandez <albert.soto@gmail.com>
2025-06-23 11:11:29 +0200

    first nx project

---
+++ decissiontree-frontend/apps/decissiontree-frontend-app/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@es.albertosoto/es.albertosoto" />
+    </div>
+  );
+}
+
+export default App;
